9c21eb15476448a5b0a65e7a62eaa3973ee49314
AlekOmOm <142972647+AlekOmOm@users.noreply.github.com>
2024-04-08 14:26:28 +0200

    day 1: startpage.html and style.css

---
+++ src/main/java/com/example/wishlist/Controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.wishlist.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/home")
+    public String home() {
+        return "startpage";
+    }
+}
