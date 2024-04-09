fba68e73583de63c5f9965e1257d8441ed096919
kasperkhansen <52578478+kasperkhansen@users.noreply.github.com>
2024-04-09 14:40:05 +0200

    small changes to WishRepo, and added fetchAll and addItem to WishService

--- src/main/java/com/example/wishlist/Repository/WishRepo.java
+++ src/main/java/com/example/wishlist/Repository/WishRepo.java
@@ -2,7 +2,9 @@ package com.example.wishlist.Repository;
 
 import com.example.wishlist.Model.Items;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,12 +15,16 @@ public class WishRepo {
     JdbcTemplate template;
 
     public List<Items> fetchAll (){
-        String sql = "SELECT * FROM ";
-        return null;
+        String sql = "SELECT * FROM Items";
+        RowMapper<Items> rowMapper = new BeanPropertyRowMapper<>(Items.class);
+        return template.query(sql, rowMapper);
     }
 
     public void addItem(Items i){
-        String sql = "INSERT INTO Items ()";
-        template.update(sql, i.getName(), i.getPrice() );
+        String sql = "INSERT INTO Items (Pname, price, i_URL) VALUES (?, ?, ?)";
+        template.update(sql, i.getName(), i.getPrice(), i.getI_URL());
     }
+
+
+
 }
