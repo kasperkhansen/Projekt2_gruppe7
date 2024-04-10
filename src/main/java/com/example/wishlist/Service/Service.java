820fd063b563192a6a5f3c49cb609bec436c2380
kasperkhansen <kasper1999.hansen@gmail.com>
2024-04-10 11:02:14 +0200

    added more to repo and to service

--- src/main/java/com/example/wishlist/Service/Service.java
+++ src/main/java/com/example/wishlist/Service/Service.java
@@ -6,7 +6,6 @@ import com.example.wishlist.Model.Wishlist;
 import com.example.wishlist.Repository.WishRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -83,6 +82,26 @@ public class Service {
     }
     public List<Item> getItems(int wishlistID) {
         return getItems();
+    }
+
+    public List<Item> fetchAll(){
+        return repo.fetchAll();
+    }
+
+    public void addWishlist(Wishlist wl, User u){
+        repo.addWishlist(wl, u);
+    }
+
+    public void addItem(Item i){
+        repo.addItem(i);
+    }
+
+    public void addUser(User u){
+        repo.addUser(u);
+    }
+
+    public void updateWishlist(Wishlist wl, int wishlistID){
+        repo.updateWishlist(wl, wishlistID);
     }
 
 }
